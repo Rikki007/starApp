eddc8de3655fd44fc7453aee29485a37f68f7508
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2025-01-06 19:49:38 +0300

    feat: add zodiac description

---
+++ zodiacDescription/zodiacDeascription.js
@@ -0,0 +1,302 @@
+const zodiacDescription = [
+  {
+    name: "Aries",
+    date: "March 21 - April 19",
+    sign: "",
+    constellation: "",
+    planet: "Mars",
+    gems: [
+      {
+        name: "Diamond",
+        positive: "Strength, clarity, determination.",
+        protection: "Negative energies, fear, doubts.",
+      },
+      {
+        name: "Ruby",
+        positive: "Passion, energy, courage.",
+        protection: "Negative emotions, insecurity.",
+      },
+      {
+        name: "Carnelian",
+        positive: "Inspiration, creativity, decisiveness.",
+        protection: "Laziness, apathy.",
+      },
+    ],
+    description: "Mars, known as the 'Red Planet,' symbolizes energy, drive, and aggression. Aries individuals, under the influence of Mars, are often dynamic leaders who are full of enthusiasm and ready to take on new challenges. They have an innate desire to initiate and lead, making them natural pioneers. Mars endows them with a fierce passion and determination, propelling them toward their goals. However, this energy can sometimes lead to impulsiveness and impatience. Despite these challenges, Mars provides Aries with the courage and motivation needed to overcome obstacles and achieve success."
+  },
+  {
+    name: "Taurus",
+    date: "April 20 - May 20",
+    sign: "",
+    constellation: "",
+    planet: "Venus",
+    gems: [
+      {
+        name: "Emerald",
+        positive: "Love, harmony, stability.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Rose Quartz",
+        positive: "Love, calmness, tenderness.",
+        protection: "Emotional wounds, fear of love.",
+      },
+      {
+        name: "Jade",
+        positive: "Luck, peace, prosperity.",
+        protection: "Negative energy, misfortune.",
+      },
+    ],
+    description: "Venus, the planet of love, beauty, and pleasure, governs Taurus. This influence makes Taurus individuals appreciate aesthetics and seek stability and comfort in their lives. They have a strong affinity for the finer things in life, such as good food, beautiful surroundings, and luxury. Venus grants them a grounded and sensual nature, which helps them enjoy life's simple pleasures. However, this can also lead to stubbornness and resistance to change. Nonetheless, Venus blesses Taurus with a loving and nurturing disposition, making them reliable and devoted partners."
+  },
+  {
+    name: "Gemini",
+    date: "May 21 - June 20",
+    sign: "",
+    constellation: "",
+    planet: "Mercury",
+    gems: [
+      {
+        name: "Agate",
+        positive: "Balance, protection, harmony.",
+        protection: "Negative energies, stress.",
+      },
+      {
+        name: "Citrine",
+        positive: "Confidence, joy, success.",
+        protection: "Negative thoughts, failures.",
+      },
+      {
+        name: "Tiger's Eye",
+        positive: "Courage, strength, protection.",
+        protection: "Fears, insecurity.",
+      },
+    ],
+    description: "Mercury, the planet of communication, intellect, and adaptability, rules Gemini. Gemini individuals are known for their curious and versatile nature, always eager to learn and share new ideas. They possess a quick wit and are excellent communicators, making them engaging conversationalists. Mercury's influence enables them to easily adapt to changing circumstances and think on their feet. However, this can also make them prone to superficiality and inconsistency. Despite this, Mercury provides Gemini with the ability to connect with others and navigate complex situations with ease."
+  },
+  {
+    name: "Cancer",
+    date: "June 21 - July 22",
+    sign: "",
+    constellation: "",
+    planet: "Moon",
+    gems: [
+      {
+        name: "Moonstone",
+        positive: "Intuition, emotional balance, protection.",
+        protection: "Negative energies, emotional stress.",
+      },
+      {
+        name: "Pearl",
+        positive: "Purity, calmness, protection.",
+        protection: "Negative energies, emotional wounds.",
+      },
+      {
+        name: "Opal",
+        positive: "Inspiration, creativity, harmony.",
+        protection: "Negative thoughts, fears.",
+      },
+    ],
+    description: "The Moon, associated with emotions, intuition, and nurturing, governs Cancer. Under the Moon's influence, Cancer individuals are deeply empathetic and sensitive. They have a strong connection to family and home, often prioritizing the well-being of their loved ones. The Moon endows them with powerful intuition and an understanding of others' emotional needs. However, this sensitivity can also make them vulnerable and prone to mood swings. Nevertheless, the Moon grants Cancer a caring and protective nature, making them devoted caregivers and friends."
+  },
+  {
+    name: "Leo",
+    date: "July 23 - August 22",
+    sign: "",
+    constellation: "",
+    planet: "Sun",
+    gems: [
+      {
+        name: "Ruby",
+        positive: "Passion, energy, courage.",
+        protection: "Negative emotions, insecurity.",
+      },
+      {
+        name: "Sunstone",
+        positive: "Vitality, joy, optimism.",
+        protection: "Depression, gloom.",
+      },
+      {
+        name: "Garnet",
+        positive: "Strength, protection, passion.",
+        protection: "Negative energies, emotional wounds.",
+      },
+    ],
+    description: "The Sun, symbolizing vitality, confidence, and self-expression, rules Leo. Leo individuals, under the Sun's influence, are charismatic and crave the spotlight. They possess a natural flair for drama and creativity, often seeking ways to showcase their talents. The Sun gives them a generous spirit and a desire to help others. However, their need for attention can sometimes lead to arrogance and egotism. Despite this, the Sun provides Leo with an inspiring presence, enabling them to shine brightly and lead with confidence."
+  },
+  {
+    name: "Virgo",
+    date: "August 23 - September 22",
+    sign: "",
+    constellation: "",
+    planet: "Mercury",
+    gems: [
+      {
+        name: "Sapphire",
+        positive: "Wisdom, honesty, purity.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Peridot",
+        positive: "Healing, cleansing, harmony.",
+        protection: "Negative energies, illnesses.",
+      },
+      {
+        name: "Amazonite",
+        positive: "Calmness, balance, confidence.",
+        protection: "Fear, stress.",
+      },
+    ],
+    description: "Mercury also rules Virgo, enhancing their analytical abilities, attention to detail, and practicality. Virgo individuals, under Mercury's influence, are methodical and organized, always striving for perfection. They have a keen eye for detail and a strong desire to improve and optimize their surroundings. Mercury endows them with a sharp intellect and a talent for critical thinking. However, this can sometimes lead to excessive criticism and perfectionism. Despite this, Mercury provides Virgo with a strong sense of duty and a willingness to help others through their skills and knowledge."
+  },
+  {
+    name: "Libra",
+    date: "September 23 - October 22",
+    sign: "",
+    constellation: "",
+    planet: "Venus",
+    gems: [
+      {
+        name: "Opal",
+        positive: "Harmony, love, beauty.",
+        protection: "Negative energies, emotional wounds.",
+      },
+      {
+        name: "Lapis Lazuli",
+        positive: "Wisdom, honesty, calmness.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Turquoise",
+        positive: "Protection, healing, peace.",
+        protection: "Negative energies, illnesses.",
+      },
+    ],
+    description: "Venus, the planet of harmony, beauty, and relationships, governs Libra. Libra individuals, under Venus's influence, strive for balance and fairness in all aspects of life. They have a natural charm and diplomacy, making them skilled negotiators and peacemakers. Venus endows them with an appreciation for art and aesthetics, as well as a desire for harmonious relationships. However, their pursuit of balance can sometimes lead to indecisiveness and avoidance of conflict. Nonetheless, Venus blesses Libra with a gracious and cooperative nature, making them excellent partners and friends"
+  },
+  {
+    name: "Scorpio",
+    date: "October 23 - November 21",
+    sign: "",
+    constellation: "",
+    planet: "Pluto and Mars",
+    gems: [
+      {
+        name: "Topaz",
+        positive: "Strength, passion, protection.",
+        protection: "Negative energies, emotional wounds.",
+      },
+      {
+        name: "Obsidian",
+        positive: "Protection, cleansing, transformation.",
+        protection: "Negative energies, evil spirits.",
+      },
+      {
+        name: "Malachite",
+        positive: "Protection, balance, transformation.",
+        protection: "Negative thoughts, stress.",
+      },
+    ],
+    description: "Pluto, associated with transformation, depth, and power, rules Scorpio alongside Mars. Under these influences, Scorpio individuals possess intense emotions and a profound desire for change and growth. They are mysterious and intuitive, often delving into the hidden aspects of life. Pluto grants them resilience and the ability to endure and overcome challenges. However, their intensity can also lead to jealousy and possessiveness. Despite this, Pluto and Mars endow Scorpio with a strong will and the ability to transform both themselves and those around them."
+  },
+  {
+    name: "Sagittarius",
+    date: "November 22 - December 21",
+    sign: "",
+    constellation: "",
+    planet: "Jupiter",
+    gems: [
+      {
+        name: "Turquoise",
+        positive: "Wisdom, protection, luck.",
+        protection: "Negative energies, illnesses.",
+      },
+      {
+        name: "Amethyst",
+        positive: "Spirituality, peace, protection.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Citrine",
+        positive: "Confidence, joy, success.",
+        protection: "Negative thoughts, failures.",
+      },
+    ],
+    description: "Jupiter, the planet of expansion, optimism, and wisdom, governs Sagittarius. Sagittarius individuals, under Jupiter's influence, are adventurous and constantly seeking new experiences and knowledge. They have an innate curiosity and a love for travel and exploration. Jupiter endows them with a positive outlook and the ability to see the bigger picture. However, their quest for freedom can sometimes lead to restlessness and a lack of focus. Despite this, Jupiter provides Sagittarius with a generous spirit and the desire to inspire and uplift others through their adventures and insights."
+  },
+  {
+    name: "Capricorn",
+    date: "December 22 - January 19",
+    sign: "",
+    constellation: "",
+    planet: "Saturn",
+    gems: [
+      {
+        name: "Garnet",
+        positive: "Stability, strength, passion.",
+        protection: "Negative energies, emotional wounds.",
+      },
+      {
+        name: "Onyx",
+        positive: "Protection, strength, confidence.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Jet",
+        positive: "Protection, healing, balance.",
+        protection: "Negative energies, illnesses.",
+      },
+    ],
+    description: "Saturn, the planet of discipline, responsibility, and ambition, rules Capricorn. Capricorn individuals, under Saturn's influence, are determined and hardworking, always striving to achieve their goals. They possess a strong sense of duty and a practical approach to life. Saturn grants them patience and the ability to plan for the long term. However, their drive for success can sometimes lead to rigidity and emotional reserve. Despite this, Saturn provides Capricorn with the resilience and perseverance needed to overcome obstacles and achieve lasting success."
+  },
+  {
+    name: "Aquarius",
+    date: "January 20 - February 18",
+    sign: "",
+    constellation: "",
+    planet: "Uranus and Saturn",
+    gems: [
+      {
+        name: "Amethyst",
+        positive: "Spirituality, peace, protection.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Aquamarine",
+        positive: "Calmness, clarity, inspiration.",
+        protection: "Negative energies, fear.",
+      },
+      {
+        name: "Garnet",
+        positive: "Strength, protection, passion.",
+        protection: "Negative energies, emotional wounds.",
+      },
+    ],
+    description: "Uranus, the planet of innovation, originality, and independence, rules Aquarius alongside Saturn. Under these influences, Aquarius individuals are progressive and eccentric, always eager to embrace new ideas and change. They value freedom and independence, often challenging the status quo. Uranus endows them with a visionary outlook and the ability to think outside the box. However, their desire for independence can sometimes lead to detachment and unpredictability. Despite this, Uranus and Saturn provide Aquarius with the creativity and determination to bring about positive change in the world."
+  },
+  {
+    name: "Pisces",
+    date: "February 19 - March 20",
+    sign: "",
+    constellation: "",
+    planet: "Neptune and Jupiter",
+    gems: [
+      {
+        name: "Aquamarine",
+        positive: "Calmness, clarity, inspiration.",
+        protection: "Negative energies, fear.",
+      },
+      {
+        name: "Amethyst",
+        positive: "Spirituality, peace, protection.",
+        protection: "Negative thoughts, stress.",
+      },
+      {
+        name: "Bloodstone",
+        positive: "Protection, healing, strength.",
+        protection: "Negative energies, illnesses.",
+      },
+    ],
+    description: "Neptune, the planet of dreams, intuition, and spirituality, governs Pisces alongside Jupiter. Pisces individuals, under Neptune's influence, are deeply empathetic and imaginative. They often find themselves lost in their dreams and fantasies, making them natural artists and creators. Neptune grants them strong intuition and the ability to understand others' hidden motives. However, their sensitivity can sometimes lead to escapism and vulnerability. Despite this, Neptune and Jupiter provide Pisces with a compassionate and creative nature, allowing them to inspire and support those around them through their empathy and artistic expression."
+  }
+]
