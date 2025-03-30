336ce2f92b1eef97f00d7bb75f5f8eaf3871e234
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2025-03-30 14:44:48 +0300

    feat: work with interaction module

--- interaction/chooseSign.js
+++ interaction/chooseSign.js
@@ -3,23 +3,36 @@ import descriptionContent from "./descriptionContent.js";
 const chooseSign = () => {
     
     const signs = document.querySelectorAll(".rose-sign");
+    let signFirst, signSecond;
     const firstSignField = document.querySelector(".sign-field__first");
     const secondSignField = document.querySelector(".sign-field__second");
+    const arrow = document.querySelector(".cycle-arrow");
     const interactionButton = document.querySelector(".interaction-button");
+    const preloader = document.querySelector('.loader-container');
 
     firstSignField.addEventListener('change', () => {
+        if (!arrow.classList.contains('rotate')) {
+            arrow.classList.add('rotate');
+        }
+        arrow.classList.add('rotate');
         signs.forEach(item => {
             item.classList.remove("rose-sign__able");
         })
-        const sign = document.querySelector(`.${firstSignField.value}-sign`);
-        sign.classList.add("rose-sign__able");
+        signFirst = document.querySelector(`.${firstSignField.value}-sign`);
+        signFirst.classList.add("rose-sign__able");
+        signSecond.classList.add("rose-sign__able");
     });
     secondSignField.addEventListener('change', () => {
+        if (!arrow.classList.contains('rotate')) {
+            arrow.classList.add('rotate');
+        }
+        arrow.classList.add('rotate');
         signs.forEach(item => {
             item.classList.remove("rose-sign__able");
         })
-        const sign = document.querySelector(`.${secondSignField.value}-sign`);
-        sign.classList.add("rose-sign__able");
+        signSecond = document.querySelector(`.${secondSignField.value}-sign`);
+        signFirst.classList.add("rose-sign__able");
+        signSecond.classList.add("rose-sign__able");
     });
 
     interactionButton.addEventListener('click', () => {
@@ -30,6 +43,13 @@ const chooseSign = () => {
             alert('Выберете знак во втором поле ввода');
         }
         if (firstSignField.value !== 'default' && secondSignField.value !== 'default') {
+            preloader.classList.toggle('loader-container_disable');
+            setTimeout(() => {
+                preloader.classList.toggle('loader-container_disable');
+            }, 1000);
+            if (arrow.classList.contains('rotate')) {
+                arrow.classList.remove('rotate');
+            }
             descriptionContent(firstSignField.value, secondSignField.value)
         }
     });
--- interaction/descriptionContent.js
+++ interaction/descriptionContent.js
@@ -13,18 +13,22 @@ const descriptionContent = (mainSign, secondarySign) => {
             <p class="common-wrapper__description">${data.Modes}.</p>
             <h3 class="common-wrapper__title">Полярности:</h3>
             <p class="common-wrapper__description">${data.Polarities}.</p>
+            <p class="common-wrapper__description">${data.Description}.</p>
         </div>
         <div class="common-wrapper">
             <h3 class="common-wrapper__title">Отношения:</h3>
             <p class="common-wrapper__description">${data.Romance}</p>
+            <p class="common-wrapper__description">Оценка взаимодействия: ${data.RomanceScore}/5</p>
             <h3 class="common-wrapper__title">Дружба:</h3>
             <p class="common-wrapper__description">${data.Friendship}</p>
+            <p class="common-wrapper__description">Оценка взаимодействия: ${data.FriendshipScore}/5</p>
             <h3 class="common-wrapper__title">Взаимодействие на работе:</h3>
             <p class="common-wrapper__description">${data.Work}</p>
+            <p class="common-wrapper__description">Оценка взаимодействия: ${data.WorkScore}/5</p>
         </div>
         <div class="common-wrapper">
             <h3 class="common-wrapper__title">Совет:</h3>
-            <p class="common-wrapper__description">${data.Advice}.</p>
+            <p class="common-wrapper__description">${data.Advice}</p>
         </div>
     `
 }
