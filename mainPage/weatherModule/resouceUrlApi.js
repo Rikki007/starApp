e17e73f36deccca2f853fbd9dc00f7ef0b5eae12
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2025-05-05 10:11:17 +0300

    fix: change hyper text protocol

--- mainPage/weatherModule/resouceUrlApi.js
+++ mainPage/weatherModule/resouceUrlApi.js
@@ -1,2 +1,2 @@
-export const url = 'http://api.weatherapi.com/v1/current.json';
+export const url = 'https://api.weatherapi.com/v1/current.json';
 export const key = 'e5bc7172de43469ab22164308250405';
