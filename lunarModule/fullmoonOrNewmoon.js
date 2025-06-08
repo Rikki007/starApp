6f4bdf32247bd26cc669c3a5b4be78aebbc84ee3
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2025-06-08 20:44:38 +0300

    fix: change data to response a full or new moon

--- lunarModule/fullmoonOrNewmoon.js
+++ lunarModule/fullmoonOrNewmoon.js
@@ -1,8 +1,8 @@
 const fullmoonOrNewmoon = (fraction) => {
     let fullmoonNewmoon;
-    if (fraction < 0.007) {
+    if (fraction < 0.004) {
         fullmoonNewmoon = 'Новолуние.';
-    } else if (fraction > 0.093) {
+    } else if (fraction > 0.096) {
         fullmoonNewmoon = 'Полнолуние.';
     } else {
         fullmoonNewmoon = ' ';
