c5ed520ccc8979da577c9ff37dcdc6de7648e9f0
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2025-06-08 20:40:30 +0300

    feat: add localization to preloader

--- preloader/selectQuote.js
+++ preloader/selectQuote.js
@@ -1,12 +1,14 @@
+import getLanguage from "../localization/localizationUtils.js";
 import quotes from "./quote-data.js"
 import randomizer from "./randomizer.js"
 
 const selectQuote = () => {
     const qouteFromList = randomizer();
+    const lang = getLanguage();
     const quote = document.querySelector(".quote");
     const author = document.querySelector(".author");
-    quote.textContent = quotes[qouteFromList][0];
-    author.textContent = quotes[qouteFromList][1];
+    quote.textContent = quotes[lang][qouteFromList][0];
+    author.textContent = quotes[lang][qouteFromList][1];
 }
 
 export default selectQuote;
