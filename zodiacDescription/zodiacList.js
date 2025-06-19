5d37516f0fc9743713a4c2e782114527115c9622
Kirill Oreshkevich <oreshaodnoglazy@gmail.com>
2025-06-19 20:05:19 +0300

    feat: work with zodiac desription module

--- zodiacDescription/zodiacList.js
+++ zodiacDescription/zodiacList.js
@@ -1,16 +1,17 @@
+import getLanguage from "../localization/localizationUtils.js";
+import localization from "./zodiacListLanguageData.js";
 import zodiacButtons from "./zodiacButtons.js";
 
 const zodiacListCreate = () => {
+    const lang = getLanguage();
     const main = document.querySelector('.main')
     main.innerHTML = `
         <div class="elements-container">
             <div class="container-item">
                 <div class="common-container">
                     <img src="./assets/images/elements/fire.png" alt="pic" class="container-item____picture">
-                    <h3 class="container-item__title">Fire</h3>
-                    <p class="container-item__description">
-                    Fire signs are known for their energy, passion, and enthusiasm. They love to act quickly and decisively, seeking new adventures and challenges. These signs are full of life, charismatic, and can inspire those around them. They possess strong willpower and a desire for leadership.
-                    </p>
+                    <h3 class="container-item__title">${localization[lang].element[0]}</h3>
+                    <p class="container-item__description">${localization[lang].fireDescription}</p>
                 </div>
                 
                 <div class="sign-elements-list">
@@ -23,10 +24,8 @@ const zodiacListCreate = () => {
             <div class="container-item">
                 <div class="common-container">
                     <img src="./assets/images/elements/water.png" alt="pic" class="container-item____picture">
-                    <h3 class="container-item__title">Water</h3>
-                    <p class="container-item__description">
-                    Water signs are characterized by deep emotions, intuition, and sensitivity. They are often very empathetic and capable of understanding the feelings of others. These signs tend to be introspective and can be very creative. Their emotions flow like water, and they often seek depth and meaning in life.
-                    </p>
+                    <h3 class="container-item__title">${localization[lang].element[1]}</h3>
+                    <p class="container-item__description">${localization[lang].waterDescription}</p>
                 </div>
                 
                 <div class="sign-elements-list">
@@ -39,10 +38,8 @@ const zodiacListCreate = () => {
             <div class="container-item">
                 <div class="common-container">
                     <img src="./assets/images/elements/earth.png" alt="pic" class="container-item____picture">
-                    <h3 class="container-item__title">Earth</h3>
-                    <p class="container-item__description">
-                    Earth signs are known for their practicality, reliability, and stability. They are focused on the material and physical aspects of life, seeking security and stability. These signs are hardworking, patient, and have a strong sense of responsibility. They prefer to act methodically and achieve their goals step by step.
-                    </p>
+                    <h3 class="container-item__title">${localization[lang].element[2]}</h3>
+                    <p class="container-item__description">${localization[lang].earthDescription}</p>
                 </div>
                 
                 <div class="sign-elements-list">
@@ -55,10 +52,8 @@ const zodiacListCreate = () => {
             <div class="container-item">
                 <div class="common-container">
                     <img src="./assets/images/elements/air.png" alt="pic" class="container-item____picture">
-                    <h3 class="container-item__title">Air</h3>
-                    <p class="container-item__description">
-                        Air signs are characterized by their intellectuality, communicativeness, and curiosity. They love to exchange ideas, discuss, and analyze various aspects of life. These signs strive for harmony and balance, often showing flexibility and adaptability. They value freedom and independence.
-                    </p>
+                    <h3 class="container-item__title">${localization[lang].element[3]}</h3>
+                    <p class="container-item__description">${localization[lang].airDescription}</p>
                 </div>
                 
                 <div class="sign-elements-list">
---
+++ zodiacDescription/zodiacListLanguageData.js
@@ -0,0 +1,28 @@
+const localization = {
+    "en": {
+        element: ["Fire", "Water", "Earth", "Air"],
+        fireDescription: "Fire signs are known for their energy, passion, and enthusiasm. They love to act quickly and decisively, seeking new adventures and challenges. These signs are full of life, charismatic, and can inspire those around them. They possess strong willpower and a desire for leadership.",
+        waterDescription: "Water signs are characterized by deep emotions, intuition, and sensitivity. They are often very empathetic and capable of understanding the feelings of others. These signs tend to be introspective and can be very creative. Their emotions flow like water, and they often seek depth and meaning in life.",
+        earthDescription: "Earth signs are known for their practicality, reliability, and stability. They are focused on the material and physical aspects of life, seeking security and stability. These signs are hardworking, patient, and have a strong sense of responsibility. They prefer to act methodically and achieve their goals step by step.",
+        airDescription: "Air signs are characterized by their intellectuality, communicativeness, and curiosity. They love to exchange ideas, discuss, and analyze various aspects of life. These signs strive for harmony and balance, often showing flexibility and adaptability. They value freedom and independence.",
+        buttons: ["Aries", "Leo", "Sagittarius", "Cancer", "Scorpio", "Pisces", "Taurus", "Virgo", "Capricorn", "Gemini", "Libra", "Aquarius"],
+    },
+    "ru": {
+        element: ["Огонь", "Вода", "Земля", "Воздух"],
+        fireDescription: "Огненные знаки отличаются энергией, страстью и энтузиазмом. Они быстро принимают решения, любят новые вызовы и стремятся к действию. Эти знаки яркие, харизматичные и вдохновляющие. У них сильная воля и стремление к лидерству.",
+        waterDescription: "Водные знаки глубоко чувствуют, обладают сильной интуицией и высокой эмпатией. Они понимают эмоции других, склонны к размышлениям и творчеству. Их чувства текут, как вода, а в жизни они ищут смысл и глубину.",
+        earthDescription: "Земные знаки практичны, надёжны и стабильны. Они ценят материальную сторону жизни и стремятся к безопасности. Эти люди терпеливы, трудолюбивы и ответственны, добиваются целей шаг за шагом.",
+        airDescription: "Воздушные знаки умны, любознательны и общительны. Они любят обмениваться идеями, обсуждать и исследовать мир. Стремятся к свободе, гибкости и равновесию, легко адаптируются к изменениям.",
+        buttons: ["Овен", "Лев", "Стрелец", "Рак", "Скорпион", "Рыбы", "Телец", "Дева", "Козерог", "Близнецы", "Весы", "Водолей"]
+    },
+    "cz": {
+        element: ["Oheň", "Voda", "Země", "Vzduch"],
+        fireDescription: "Ohňová znamení jsou plná energie, vášně a nadšení. Jednají rychle a odvážně, milují výzvy a nové zážitky. Jsou charismatická a inspirují své okolí. Mají silnou vůli a touhu vést.",
+        waterDescription: "Vodní znamení se vyznačují hlubokými emocemi, intuicí a citlivostí. Jsou empatická a rozumějí pocitům druhých. Často jsou tvůrčí a hledají smysl a hloubku ve svém životě.",
+        earthDescription: "Zemská znamení jsou praktická, spolehlivá a stabilní. Zaměřují se na hmotné a reálné věci. Jsou trpělivá, pracovitá a zodpovědná. Cení si jistoty a systematického postupu.",
+        airDescription: "Vzdušná znamení jsou intelektuální, komunikativní a zvídavá. Ráda sdílejí myšlenky, diskutují a analyzují svět kolem sebe. Milují svobodu a dokážou se snadno přizpůsobit.",
+        buttons: ["Beran", "Lev", "Střelec", "Rak", "Štír", "Ryby", "Býk", "Panna", "Kozoroh", "Blíženci", "Váhy", "Vodnář"]
+    }
+}
+
+export default localization;
